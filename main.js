651a2407aaaadc3941860c28c22bd2e66f193dc1
jonah <jonahhb@umich.edu>
2023-03-09 15:53:11 -0500

    navbar burger

--- main.js
+++ main.js
@@ -101,4 +101,29 @@ window.onload = function() {
     document.getElementById("generate").addEventListener("click", name);
     //allow enter key to trigger generate
     document.getElementById("name").addEventListener("keyup", event);
+
+    //navbar 
 }
+
+//navbar code for burger menu for mobile
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+});
