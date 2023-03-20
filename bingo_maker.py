aae7ba59398c40a62ba5088093c2338460df8fa0
Eric Andrechek <eric@andrechek.com>
2023-03-20 10:16:11 -0400

    added margins to pdf generated

--- bingo_maker.py
+++ bingo_maker.py
@@ -13,21 +13,22 @@ class Bingo():
         self.local_dir = ""
         #create the pdf
         self.pdf = FPDF(orientation = 'P', unit = 'in', format='Letter')
+        self.pdf.set_margins(left = 0.75, top = 0.75, right = 0.75)
+        self.pdf.set_auto_page_break(auto = True, margin = 0.75)
+        self.pdf.set_author('F1 Tools')
         self.pdf.add_page()
 
         #Place the template image to take the size of the page and center it
-        self.pdf.image(self.local_dir + 'template.png', x = 0.25, y = 0.25, w = 8, h = 10.5, type = '', link = '')
+        self.pdf.image(self.local_dir + 'template.png', x = 0.75, y = 0.75, w = 7, h = 9, type = '', link = '')
 
 
         # list of all the possible bingo squares locations and size of the square
         # access is by LOCATIONS[square#][0] for x and LOCATIONS[square#][1] for y
         # the square# is 0-24 for the 25 squares working left to right, top to bottom
-        self.LOCATIONS = [(0.32,2.45),(1.92,2.45),(3.51,2.45),(5.10,2.45),(6.69,2.45),
-                    (0.32,4.1),(1.92,4.1),(3.51,4.1),(5.10,4.1),(6.69,4.1),
-                    (0.32,5.75),(1.92,5.75),(3.51,5.75),(5.10,5.75),(6.69,5.75),
-                    (0.32,7.45),(1.92,7.45),(3.51,7.45),(5.10,7.45),(6.69,7.45),
-                    (0.32,9.1),(1.92,9.1),(3.51,9.1),(5.10,9.1),(6.69,9.1)]
-        self.SQUARE_SIZE = 1.5
+        x_es = [0.8, 2.2, 3.6, 5, 6.4]
+        y_es = [2.6, 4.05, 5.5, 6.9, 8.35]
+        self.LOCATIONS = [(x_es[i], y_es[j]) for j in range(5) for i in range(5)]
+        self.SQUARE_SIZE = 1.3
 
         # List of which squares are taken so we don't place two images in the same square
         # access is by taken[square#] where square# is 0-24 for the 25 squares
@@ -67,7 +68,8 @@ class Bingo():
         elif step >= 4 and step < 25:
             if step == 4:
                 #get a list of all the images in the imgs directory
-                self.imgs = os.listdir(self.local_dir + 'imgs')
+                self.imgs = os.listdir(self.local_dir + 'imgs/')
+                self.imgs = [i for i in self.imgs if ".png" in i]
                 #remove the images we have already placed
                 self.imgs.remove('racestarts.png')
                 self.imgs.remove('redflag.png')
@@ -92,10 +94,12 @@ class Bingo():
 
 
     def name(self, name):
-        self.pdf.text(0.32, 0.2, name)
+        self.pdf.text(0.8, 0.65, name)
     
     def save(self):
         #output the bingo card to the current directory
+        self.pdf.set_xy(0.8, 9.9)
+        self.pdf.write(0.0, "f1-tools.github.io", "https://f1-tools.github.io/")
         self.pdf.output(self.local_dir + 'bingo_card.pdf', 'F')
     
     def base64_export(self):
